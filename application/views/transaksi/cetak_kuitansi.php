4d0de5e876144a8b37be8eec05122349063f616d
mustofa27 <buffonmustofa@gmail.com>
2015-03-15 07:41:33 +0700

    Sudah fix bre !!

--- application/views/transaksi/cetak_kuitansi.php
+++ application/views/transaksi/cetak_kuitansi.php
@@ -11,11 +11,11 @@ jika ingin mensetting sendiri format halamannya, gunakan array(width, height)
 contoh : $this->fpdf->FPDF("P","cm", array(20, 20));
 */
 
-$this->fpdf->FPDF("P","cm","A4");
+$this->fpdf->FPDF("P","cm",array(21,15));
 
 // kita set marginnya dimulai dari kiri, atas, kanan. jika tidak diset, defaultnya 1 cm
 $this->fpdf->SetMargins(1,1,1);
-
+$this->fpdf->SetAutoPageBreak(true , 1);
 
 /* AliasNbPages() merupakan fungsi untuk menampilkan total halaman
 di footer, nanti kita akan membuat page number dengan format : number page / total page
@@ -60,48 +60,59 @@ $this->fpdf->Line(1,3.55,20,3.55);
 /* -------------- Header Halaman selesai ------------------------------------------------*/
 
 $this->fpdf->Ln(1);
+$this->fpdf->SetFont('Times','',10);
+$this->fpdf->Cell(19,0.1,$records['tanggal_transaksi'],0,0,'R');
+$this->fpdf->Ln(0.1);
 $this->fpdf->SetFont('Times','B',12);
 $this->fpdf->Cell(19,1,'NOTA PEMBAYARAN',0,0,'C');
 $this->fpdf->Ln();
+$this->fpdf->SetFont('Times','',9);
+$this->fpdf->Cell(9.7,0.1,'Transaksi no : ',0,0,'R');
+$this->fpdf->Cell(9,0.1,$records['id_transaksi'],0,0,'L');
+$this->fpdf->Ln(1.5);
 $total1 = 0;
 /* setting header table */
-$this->fpdf->SetFont('Times','',12);
-$this->fpdf->Cell(3 , 0.7, 'ID Transaksi' , 0, 0, 'L');
-$this->fpdf->Cell(1 , 0.7, ':' , 0, 0, 'C');
-$this->fpdf->Cell(3 , 0.7, $records["id_transaksi"] , 0, 0, 'L');
+$this->fpdf->SetFont('Times','B',14);
+$this->fpdf->Cell(2 , 0.5, 'No'           , 1, 'LTR', 'C');
+$this->fpdf->Cell(4 , 0.5, 'ID Layanan'           , 1, 'TR', 'C');
+$this->fpdf->Cell(8 , 0.5, 'Nama Layanan' , 1, 'TR', 'C');
+$this->fpdf->Cell(5 , 0.5, 'Harga' , 1, 'TR', 'C');
+$this->fpdf->SetFont('Times','',13);
+$no = 1;
 $this->fpdf->Ln();
-$this->fpdf->Cell(3 , 0.7, 'Nama Pasien' , 0, 0, 'L');
-$this->fpdf->Cell(1 , 0.7, ':' , 0, 0, 'C');
-$this->fpdf->Cell(4, 0.7, $records["nama_pasien"] , 0, 0, 'L');
-$this->fpdf->Ln();
-$this->fpdf->Cell(3 , 0.7, 'Nama Dokter' , 0, 0, 'L');
-$this->fpdf->Cell(1 , 0.7, ':' , 0, 0, 'C');
-$this->fpdf->Cell(4, 0.7, $records["nama_dokter"] , 0, 0, 'L');
-$this->fpdf->Ln();
-$this->fpdf->Cell(3 , 0.7, 'Layanan' , 0, 0, 'L');
-$this->fpdf->Cell(1 , 0.7, ':' , 0, 0, 'C');
 foreach ($records["layanan"] as $layanan) {
-$this->fpdf->Cell(6 , 0.7, $layanan["nama_layanan"], 0, 0, 'L');
-$this->fpdf->Cell(4 , 0.7, $layanan["tarif_layanan"], 0, 0, 'L');
-$this->fpdf->Ln();
-$this->fpdf->Cell(3 , 0.7, '', 0, 0, 'L');
-$this->fpdf->Cell(1 , 0.7, '', 0, 0, 'L');
-$total1+=$layanan["tarif_layanan"];
+  $this->fpdf->Cell(2 , 0.5, $no, 1, 'LTR', 'C');
+  $this->fpdf->Cell(4 , 0.5, $layanan["id_layanan"], 1, 'TR', 'C');
+  $this->fpdf->Cell(8 , 0.5, $layanan["nama_layanan"], 1, 'TR', 'C');
+  $this->fpdf->Cell(5 , 0.5, 'Rp '.$layanan["tarif_layanan"].',00', 1, 'R', 'C');
+  $this->fpdf->Ln();
+  $total1+=$layanan["tarif_layanan"];
+  $no++;
 }
-$this->fpdf->SetFont('Times','B',12);
-$this->fpdf->Cell(6 , 0.7, 'Total Pembayaran', 0, 'L', 'L');
-$this->fpdf->Cell(4 , 0.7, $total1, 0, 'L', 'L');
+$this->fpdf->SetFont('Times','B',13);
+$this->fpdf->Cell(14 , 0.5, 'Total Pembayaran', 1, 'LTR', 'C');
+$this->fpdf->Cell(5 , 0.5, 'Rp '.$total1.',00', 1, 'T', 'C');
+$this->fpdf->Ln();
+$this->fpdf->Ln(1.5);
+
 /* setting posisi footer 3 cm dari bawah */
-$this->fpdf->SetY(-3);
+$this->fpdf->SetXY(-7,-2.5);
+$this->fpdf->SetFont('Times','',10);
+$this->fpdf->Cell(4 , 0.1, 'Dokter yang melayani  : '.' '.$records["nama_dokter"], 0, 0, 'L');
+$this->fpdf->Ln();
+$this->fpdf->SetXY(-7,-2);
+$this->fpdf->Cell(7 , 0.1, 'Pasien yang dilayani    : '.' '.$records["nama_pasien"], 0, 0, 'L');
+$this->fpdf->Ln();
+$this->fpdf->SetY(-1.5);
 
 /* setting font untuk footer */
 $this->fpdf->SetFont('Times','',10);
 
 /* setting cell untuk waktu pencetakan */
-$this->fpdf->Cell(9.5, 0.5, 'Printed on : '.date('d/m/Y H:i').' | Created by : Gus Hanafi',0,'LR','L');
+$this->fpdf->Cell(9.5, 0.1, 'Printed on : '.date('d/m/Y H:i').' | Created by : Gus Hanafi',0,'LR','L');
 
 /* setting cell untuk page number */
-$this->fpdf->Cell(9.5, 0.5, 'Page '.$this->fpdf->PageNo().'/{nb}',0,0,'R');
+//$this->fpdf->Cell(9.5, 0.5, 'Page '.$this->fpdf->PageNo().'/{nb}',0,0,'R');
 
 /* generate pdf jika semua konstruktor, data yang akan ditampilkan, dll sudah selesai */
 $this->fpdf->Output("data_pasien.pdf","I");
